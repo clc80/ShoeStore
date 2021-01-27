80a3bd17a278c463499b350ac1db1a53c8b45f9e
Claudia Maciel <clc_80@yahoo.com>
2021-01-26 22:47:12 -0800

    Added Shoe data model

---
+++ ShoeStore/Model/Shoes.swift
@@ -0,0 +1,22 @@
+//
+//  Shoes.swift
+//  ShoeStore
+//
+//  Created by Claudia Maciel on 1/26/21.
+//
+
+import Foundation
+
+struct Shoe {
+    private(set) public var name: String
+    private(set) public var description: String
+    private(set) public var price: Int
+    private(set) public var imageName: String
+    
+    init(name: String, description: String, price: Int, imageName: String) {
+        self.name = name
+        self.description = description
+        self.price = price
+        self.imageName = imageName
+    }
+}
