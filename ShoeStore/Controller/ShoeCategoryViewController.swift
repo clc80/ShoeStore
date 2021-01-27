c10621ce88e4c1439d788610564b61a56c217e8b
Claudia Maciel <clc_80@yahoo.com>
2021-01-26 23:57:51 -0800

    Shoe Category View Controller Finished

--- ShoeStore/Controller/ShoeCategoryViewController.swift
+++ ShoeStore/Controller/ShoeCategoryViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ShoeCategoryViewController: UIViewController {
+class ShoeCategoryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    @IBOutlet var shoeCategoryView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        shoeCategoryView.delegate = self
+        shoeCategoryView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return DataService.instance.getCategories().count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath) as? CategoryCollectionViewCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCollectionViewCell()
+        }
     }
 
 
 }
-
